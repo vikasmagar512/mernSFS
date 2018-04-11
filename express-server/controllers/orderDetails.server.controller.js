74b3901941e8a1b333339ec899ac6fb801662436
vikas magar <vikasmagar512@gmail.com>
2018-04-12 02:05:24 +0530

    code cleaning

--- express-server/controllers/orderDetails.server.controller.js
+++ express-server/controllers/orderDetails.server.controller.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 
 //import models
 import orderDetails from '../models/order.server.model';
-import * as OrderDetailController from "./orderDetails.server.controller";
 
 export const getOrders = (req,res) => {
     orderDetails.find().exec((err,orderDetails) => {
@@ -86,38 +85,3 @@ export const getOrderDetailByAccessoryId = (req,res) => {
         }
     })
 }
-
-/*
-export const deleteTodo = (req,res) => {
-  Todo.findByIdAndRemove(req.params.id, (err,todo) => {
-    if(err){
-    return res.json({'success':false,'message':'Some Error'});
-    }
-
-    return res.json({'success':true,'message':todo.todoText+' deleted successfully'});
-  })
-}
-
-
-export const addTodo = (req,res) => {
-    console.log(req.body);
-    const newTodo = new Todo(req.body);
-    newTodo.save((err,todo) => {
-        if(err){
-            return res.json({'success':false,'message':'Some Error'});
-        }
-
-        return res.json({'success':true,'message':'Todo added successfully',todo});
-    })
-}
-
-export const updateTodo = (req,res) => {
-    Todo.findOneAndUpdate({ _id:req.body.id }, req.body, { new:true }, (err,todo) => {
-        if(err){
-            return res.json({'success':false,'message':'Some Error','error':err});
-        }
-        console.log(todo);
-        return res.json({'success':true,'message':'Updated successfully',todo});
-    })
-}
-*/
--- express-server/controllers/stockDetails.server.controller.js
+++ express-server/controllers/stockDetails.server.controller.js
@@ -72,38 +72,3 @@ export const getStockDetailByAccessoryId = (req,res) => {
         }
     })
 }
-
-/*
-export const deleteTodo = (req,res) => {
-  Todo.findByIdAndRemove(req.params.id, (err,todo) => {
-    if(err){
-    return res.json({'success':false,'message':'Some Error'});
-    }
-
-    return res.json({'success':true,'message':todo.todoText+' deleted successfully'});
-  })
-}
-
-
-export const addTodo = (req,res) => {
-    console.log(req.body);
-    const newTodo = new Todo(req.body);
-    newTodo.save((err,todo) => {
-        if(err){
-            return res.json({'success':false,'message':'Some Error'});
-        }
-
-        return res.json({'success':true,'message':'Todo added successfully',todo});
-    })
-}
-
-export const updateTodo = (req,res) => {
-    Todo.findOneAndUpdate({ _id:req.body.id }, req.body, { new:true }, (err,todo) => {
-        if(err){
-            return res.json({'success':false,'message':'Some Error','error':err});
-        }
-        console.log(todo);
-        return res.json({'success':true,'message':'Updated successfully',todo});
-    })
-}
-*/
